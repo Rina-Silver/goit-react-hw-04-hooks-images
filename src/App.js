ec75f24dd1fa1207312048ae95dd975a1d39c971
Унжакова Екатерина <katerina.unz@gmail.com>
2021-11-29 00:42:42 +0700

    Создание компонента App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React, { Component } from 'react';
+import s from './App.module.css';
+import Searchbar from 'components/Searchbar';
 
+//rcc
+class App extends Component {
+  state = {
+    images: [],
+    query: '',
+    page: 1,
+    showModal: false,
+    showSpinner: false,
+  };
+
+  render() {
+    return (
+      <div className={s.App}>
+        <Searchbar />
+      </div>
+    );
+  }
+}
 export default App;
+
+//  <ImageGallery />
+//         <Loader />
+//         <Button />
+//         <Modal />
