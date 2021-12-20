6b8ddec0e4fba92fe8e17fb12624854c49e34fca
Унжакова Екатерина <katerina.unz@gmail.com>
2021-12-20 21:37:25 +0700

    Переделана модалка на хуки

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,42 +1,75 @@
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import s from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export default class Modal extends Component {
-  static propTypes = {
-    onClose: PropTypes.func.isRequired,
-    imageModal: PropTypes.string,
-  };
+export default function Modal({ onClose, imageModal }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleKeyDown]);
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
+  function handleKeyDown(e) {
     if (e.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
-  };
-  handleBackdropClick = e => {
-    if (e.currentTarget === e.target) {
-      this.props.onClose();
-    }
-  };
-
-  render() {
-    return createPortal(
-      <div className={s.Overlay} onClick={this.handleBackdropClick}>
-        <div className={s.Modal}>
-          <img src={this.props.imageModal} alt="" />
-        </div>
-      </div>,
-      modalRoot,
-    );
   }
+  const handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      onClose();
+    }
+  };
+  return createPortal(
+    <div className={s.Overlay} onClick={handleBackdropClick}>
+      <div className={s.Modal}>
+        <img src={imageModal} alt="" />
+      </div>
+    </div>,
+    modalRoot,
+  );
 }
+
+Modal.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  imageModal: PropTypes.string.isRequired,
+};
+
+// export default class Modal extends Component {
+//   static propTypes = {
+//     onClose: PropTypes.func.isRequired,
+//     imageModal: PropTypes.string,
+//   };
+
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+//   handleBackdropClick = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <div className={s.Overlay} onClick={this.handleBackdropClick}>
+//         <div className={s.Modal}>
+//           <img src={this.props.imageModal} alt="" />
+//         </div>
+//       </div>,
+//       modalRoot,
+//     );
+//   }
+// }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,59 +1,63 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import s from './Searchbar.module.css';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export default class Searchbar extends Component {
-  state = {
-    value: '',
-  };
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
-  };
-
-  handleValueChange = e => {
-    this.setState({ value: e.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    const { value } = this.state;
-    if (value.trim() === '') {
-      // console.log('Не введено значение');
-      return toast.error('Не введено значение', {
-        position: 'top-right',
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-    }
-    this.props.onSubmit(value);
-    this.setState({ value: '' });
-  };
-
-  render() {
-    return (
-      <header className={s.Searchbar}>
-        <form className={s.SearchForm} onSubmit={this.handleSubmit}>
-          <button type="submit" className={s.SearchForm_button}>
-            <span className={s.SearchForm_button_label}>🐌</span>
-          </button>
-
-          <input
-            className={s.SearchForm_input}
-            type="text"
-            placeholder="Search images and photos"
-            value={this.state.value}
-            autoComplete="off"
-            autoFocus
-            onChange={this.handleValueChange}
-          />
-        </form>
-      </header>
-    );
-  }
+export default function Searchbar() {
+  return <div></div>;
 }
+
+// export default class Searchbar extends Component {
+//   state = {
+//     value: '',
+//   };
+//   static propTypes = {
+//     onSubmit: PropTypes.func.isRequired,
+//   };
+
+//   handleValueChange = e => {
+//     this.setState({ value: e.currentTarget.value.toLowerCase() });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     const { value } = this.state;
+//     if (value.trim() === '') {
+//       // console.log('Не введено значение');
+//       return toast.error('Не введено значение', {
+//         position: 'top-right',
+//         autoClose: 5000,
+//         hideProgressBar: false,
+//         closeOnClick: true,
+//         pauseOnHover: true,
+//         draggable: true,
+//         progress: undefined,
+//       });
+//     }
+//     this.props.onSubmit(value);
+//     this.setState({ value: '' });
+//   };
+
+//   render() {
+//     return (
+//       <header className={s.Searchbar}>
+//         <form className={s.SearchForm} onSubmit={this.handleSubmit}>
+//           <button type="submit" className={s.SearchForm_button}>
+//             <span className={s.SearchForm_button_label}>🐌</span>
+//           </button>
+
+//           <input
+//             className={s.SearchForm_input}
+//             type="text"
+//             placeholder="Search images and photos"
+//             value={this.state.value}
+//             autoComplete="off"
+//             autoFocus
+//             onChange={this.handleValueChange}
+//           />
+//         </form>
+//       </header>
+//     );
+//   }
+// }
